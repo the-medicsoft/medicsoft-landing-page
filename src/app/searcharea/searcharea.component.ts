220939967121638d52832e24c8daedbab74ec2fc
Amandeep Rikhi <amandeeprikhi@gmail.com>
2019-10-17 17:26:24 +0530

    adding material angular and bootstrap

---
+++ src/app/searcharea/searcharea.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searcharea',
+  templateUrl: './searcharea.component.html',
+  styleUrls: ['./searcharea.component.sass']
+})
+export class SearchareaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
